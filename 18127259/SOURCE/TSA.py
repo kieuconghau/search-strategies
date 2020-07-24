be0fe220db3f2ff615882946ed2ef302a1ca30a1
Kieu Cong Hau <kieuconghau2000@gmail.com>
2020-07-24 23:48:01 +0700

    Fix bug TSA: get path from explored

--- 18127259/SOURCE/TSA.py
+++ 18127259/SOURCE/TSA.py
@@ -13,24 +13,28 @@ class V(enum.Enum):
 
 
 def tree_search_a(graph, start, goal):
-    node = (manhattan_distance(graph, start, goal), start, None)    # node = (Manhattan distance, state, parent's state)
+    node = (manhattan_distance(graph, start, goal), (start, 0), (None, 0))    # node = (Manhattan distance, state, parent's state)
     frontier = queue.PriorityQueue()
     explored = []
+    frequency = dict()
+    for state in graph:
+        frequency[state] = 0
 
     frontier.put(node)
     while frontier:
         node = frontier.get()
         explored.append((node[1], node[2]))
 
-        if node[1] == goal:
-            return len(explored), [node[0] for node in explored], get_path(explored)
+        if node[1][0] == goal:
+            return len(explored), [node[0][0] for node in explored], get_path(explored)
 
-        child_state_list = sorted(graph[node[1]])
+        child_state_list = sorted(graph[node[1][0]])
         for child_state in child_state_list:
-            if child_state != node[2]:
-                h_state = manhattan_distance(graph, node[1], goal)
+            if child_state != node[2][0]:
+                h_state = manhattan_distance(graph, node[1][0], goal)
                 h_child_state = manhattan_distance(graph, child_state, goal)
-                frontier.put((node[0] - h_state + 1 + h_child_state, child_state, node[1]))
+                frontier.put((node[0] - h_state + 1 + h_child_state, (child_state, frequency[child_state]), node[1]))
+                frequency[child_state] += 1
 
     return None, None, None
 
@@ -41,11 +45,11 @@ def get_path(explored):
         parent_table[node[0]] = node[1]
 
     state, parent_state = explored[-1][0], explored[-1][1]
-    path = deque([state])
-    while parent_state is not None:
+    path = deque([state[0]])
+    while parent_state != (None, 0):
         state = parent_state
         parent_state = parent_table[state]
-        path.appendleft(state)
+        path.appendleft(state[0])
 
     return list(path)
 
--- 18127259/SOURCE/main.py
+++ 18127259/SOURCE/main.py
@@ -7,7 +7,7 @@ from GBFS import *
 from TSA import *
 from IOHandle import *
 
-input_file_name = r"../INPUT/maze.txt"
+input_file_name = r"../INPUT/maze_0.txt"
 
 graph, start, goal = read_maze(input_file_name)
 
@@ -20,11 +20,11 @@ else:
     time, explored_nodes, path = uniform_cost_search(graph, start, goal)
     print_solution("Uniform-cost search", start, goal, time, explored_nodes, path)
 
+    time, explored_nodes_list, path = iterative_deepening_search(graph, start, goal)
+    print_solution_ids(start, goal, time, explored_nodes_list, path)
+
     time, explored_nodes, path = greedy_best_first_search(graph, start, goal)
     print_solution("Greedy-best first search", start, goal, time, explored_nodes, path)
 
     time, explored_nodes, path = tree_search_a(graph, start, goal)
     print_solution("Tree-search A*", start, goal, time, explored_nodes, path)
-
-    time, explored_nodes_list, path = iterative_deepening_search(graph, start, goal)
-    print_solution_ids(start, goal, time, explored_nodes_list, path)
